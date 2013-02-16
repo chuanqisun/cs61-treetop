75de4c4ed82d7e58e9e0bd46f7bfb9105add1e2d
Chuanqi Sun <henrysun918@gmail.com>
2013-02-16 17:03:52 -0500

    x

--- startsession.php
+++ startsession.php
@@ -3,15 +3,13 @@
 
 	//auto re-login by cookie
 	if(!isset($_SESSION['user_id'])){
-		if(isset($_COOKIE['user_id']) ){
+		if(isset($_COOKIE['user_id'])){
 			$_SESSION['user_id']=$_COOKIE['user_id'];
 			$_SESSION['username']=$_COOKIE['username'];
 			$_SESSION['account_type']=$_COOKIE['account_type'];
-            echo 'id good';
         }
-        if (isset($_COOKIE['passenger_id']){
+        if(isset($_COOKIE['passenger_id']){
 		    $_SESSION['passenger_id']=$_COOKIE['passenger_id']; 
-            echo 'pass gppd';
         }
 	}
 ?>
