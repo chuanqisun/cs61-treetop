b52038b7ad580df989d296a26dc197a818dee216
henrysun918 <henrysun918@gmail.com>
2013-02-16 01:13:08 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -20,5 +20,5 @@
 	setcookie('u_name', '', time()-3600);
 	
 	$home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/index.php';
-	//header('Location: ' . $home_url);
+	header('Location: ' . $home_url);
 ?>
