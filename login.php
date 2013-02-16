34e4c6d1cd03e9e5c049acd4a0e5205b34bc9a47
henrysun918 <henrysun918@gmail.com>
2013-02-16 00:51:05 -0500

    Update login.php

--- login.php
+++ login.php
@@ -27,7 +27,7 @@
 				echo '<p class=error">'.$username.'</p>';
 				echo '<p class=error">'.$password.'</p>';
 				echo '<p class=error">'.sha1($password).'</p>';
-				$query="SELECT user_id, username FROM account WHERE username='$username' AND password=sha1($password);";
+				$query="SELECT user_id, username FROM account WHERE username='$username' AND password=".sha1($password).";";
 				$data=mysql_query($dbc, $query);
 				echo '<p class=error">'.$query.'</p>';
 				echo '<p class=error">'.$data.'</p>';
