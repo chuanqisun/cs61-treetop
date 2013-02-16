aaeeb60e282adb1ee765b09ecb348becb496c83e
Chuanqi Sun <henrysun918@gmail.com>
2013-02-16 16:18:20 -0500

    x

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+	  
+	//start session
+	require_once('startsession.php');
+	
+	//Insert header
+	$page_title='Home';
+	require_once('header.php');
+
+	//Insert nabigation menu
+	require_once('navmenu.php');
+?>
+
+
+<?php
+	//do something here
+?>
+
+<?php
+
+	//insert footer
+	require_once('footer.php');
+?>
