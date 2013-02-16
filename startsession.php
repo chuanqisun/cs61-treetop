72a1c843ac5d9134bb5d1df31496844790fcdfa7
Chuanqi Sun <henrysun918@gmail.com>
2013-02-16 16:56:10 -0500

    x

--- startsession.php
+++ startsession.php
@@ -2,14 +2,14 @@
 	session_start();
 
 	//auto re-login by cookie
-	if(!isset($_SESSION['u_id'])){
-		if(isset($_COOKIE['u_id']) ){
-			$_SESSION['u_id']=$_COOKIE['u_id'];
-			$_SESSION['u_name']=$_COOKIE['u_name'];
-			$_SESSION['account_type'] = $_COOKIE['account_type'];
+	if(!isset($_SESSION['user_id'])){
+		if(isset($_COOKIE['user_id']) ){
+			$_SESSION['user_id']=$_COOKIE['user_id'];
+			$_SESSION['username']=$_COOKIE['username'];
+			$_SESSION['account_type']=$_COOKIE['account_type'];
         }
         if (isset($_COOKIE['passenger_id']){
-		    $_SESSION['passenger_id'] = $_COOKIE['passenger_id']; 
+		    $_SESSION['passenger_id']=$_COOKIE['passenger_id']; 
         }
 	}
 ?>
